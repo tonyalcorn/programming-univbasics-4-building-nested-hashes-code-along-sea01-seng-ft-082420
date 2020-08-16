f7f41fda364bad8db349ea3b51d14364df41b8fc
Anthony Alcorn <xmastony@gmail.com>
2020-08-16 01:00:41 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,9 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch => {},
+      :matriach => {},
+      :hero => {}.
+      :her_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
       #your key/value pairs here!
       }}
 
